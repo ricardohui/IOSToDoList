b6d527961ae2f52c1b9cef8a2023c6ab0afb97e2
Ricardo <eehaha@gmail.com>
2019-03-03 08:09:01 +0800

    Initial Commit

---
+++ ToDoList/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ToDoList
+//
+//  Created by Ricardo Hui on 3/3/2019.
+//  Copyright © 2019 Ricardo Hui. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ToDoList/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ToDoList
+//
+//  Created by Ricardo Hui on 3/3/2019.
+//  Copyright © 2019 Ricardo Hui. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
