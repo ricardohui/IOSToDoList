e0de6251c594d39d1fb3b736475394114f54216d
Ricardo <eehaha@gmail.com>
2019-03-03 10:22:26 +0800

    Working

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -8,11 +8,43 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    var items: [String] = []
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        
+ 
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    @IBOutlet var table: UITableView!
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
+        
+        
+    }
+    override func viewDidAppear(_ animated:Bool){
+        let itemObject  = UserDefaults.standard.object(forKey: "items")
+        
+        if let tempItems = itemObject as? [String]{
+            items = tempItems
+            
+        }
+        table.reloadData()
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete{
+            items.remove(at: indexPath.row)
+            table.reloadData()
+            UserDefaults.standard.set(items,forKey:"items")
+        }
+        
     }
 
 
--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    
+    @IBAction func add(_ sender: Any) {
+        let itemObject  = UserDefaults.standard.object(forKey: "items")
+       var items: [String]
+        if let tempItems = itemObject as? [String]{
+            items = tempItems
+          
+            items.append(itemTextField.text!)
+        }else{
+            items = [itemTextField.text!]
+        }
+        
+        UserDefaults.standard.set(items,forKey: "items")
+        itemTextField.text = ""
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
